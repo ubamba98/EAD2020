34731207881c69b7520b98078aea3a18ed0c325c
rishabh-16 <akchitra99@gmail.com>
2020-01-26 23:16:00 +0530

    added dice loss

--- utils.py
+++ utils.py
@@ -69,3 +69,25 @@ class BCEDiceLoss(nn.Module):
             ignore_channels=self.ignore_channels,
         )
         return dice, bce
+
+class DiceLoss(nn.Module):
+    __name__ = 'dice_loss'
+
+    def __init__(self, eps=1e-7, beta=1., activation='sigmoid', ignore_channels=None, threshold=0.5):
+        super().__init__()
+        self.beta = beta
+        self.eps = eps
+        self.threshold = threshold
+        self.ignore_channels = ignore_channels
+        self.activation = smp.utils.base.Activation(activation)
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        dice = 1 - smp.utils.functional.f_score(
+            y_pr, y_gt,
+            beta=self.beta,
+            eps=self.eps,
+            threshold=self.threshold,
+            ignore_channels=self.ignore_channels,
+        )
+        return dice
