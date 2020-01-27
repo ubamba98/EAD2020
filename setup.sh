6948b42f7423c658062af72c7e7694682c801617
AryanRaj315 <aryanraj.18je0169@am.iitism.ac.in>
2020-01-27 13:35:46 +0000

    Added Multi model training

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 pip install gdown 
+pip install tifffile
+pip install albumentations
+pip install segmentation_models_pytorch
 gdown 'https://drive.google.com/uc?id=1yiqzoWtSGtR18TkHBcGhYkScS02R3vKH&export=download'
 unzip -q EndoCV.zip -d EndoCV
 rm -r EndoCV.zip
